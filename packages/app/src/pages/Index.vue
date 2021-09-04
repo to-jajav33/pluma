75845c8443b97480889ce9f08f8aca850ab224b2
Jajav33 <to.jajav33@gmail.com>
2021-09-04 15:43:01 -0700

    refactor(app): add new quasar adn tiptap

---
+++ packages/app/src/pages/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="row items-start justify-evenly">
+    <div class="column col">
+      test
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+  components: { },
+  setup() {
+    return { };
+  }
+});
+</script>
