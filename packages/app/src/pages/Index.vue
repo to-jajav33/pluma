69390fee5321666edcd2e0c0a92cb82858f13177
Jajav33 <to.jajav33@gmail.com>
2021-09-04 16:11:52 -0700

    feat(app): add tip tap

--- packages/app/src/pages/Index.vue
+++ packages/app/src/pages/Index.vue
@@ -1,19 +1,35 @@
 <template>
   <q-page class="row items-start justify-evenly">
     <div class="column col">
-      test
+      <div ref="refsEditorWrap" class="element"></div>
     </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { Editor } from '@tiptap/core';
+import StarterKit from '@tiptap/starter-kit';
+import { defineComponent, ref, onMounted } from 'vue';
 
 export default defineComponent({
   name: 'PageIndex',
   components: { },
   setup() {
-    return { };
+    let refsEditorWrap = ref<Element|undefined>(undefined)
+    let editor : Editor | null = null;
+
+    onMounted(() => {
+      editor = new Editor({
+        element: refsEditorWrap?.value,
+        extensions: [StarterKit],
+        content: '<p>Hello World!</p>'
+      })
+    })
+
+    return {
+      editor,
+      refsEditorWrap
+    };
   }
 });
 </script>
